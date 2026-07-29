cf2bc2065d393d601c75af2139d2e1154828507e
Jun Hong <junh161108@gmail.com>
2026-07-29 17:21:27 +0800

    .

---
+++ ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/29 15:35:52 by juho              #+#    #+#             */
+/*   Updated: 2026/07/29 16:10:34 by juho             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memcopy(void *dest, cons void *src, size_t n)
+{
+	unsigned = *ptr;
+	
+	size_t i;
+	
+	unsigned char *d = (unsigned char *)dest;
+    const unsigned char *s = (const unsigned char *)src;
+	i = 0;
+	
+	
+	
+}
