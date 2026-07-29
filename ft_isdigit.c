56cf130869187b223a33a8a3e370e052ab2bc3bb
Jun Hong <junh161108@gmail.com>
2026-07-29 14:50:37 +0800

    .

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,6 +1,6 @@
 int ft_isdigit(int nb)
 {
-    if (nb >= '1' && nb <= '9')
+    if (nb >= '0' && nb <= '9')
         return(1);
     else
         return(0);
--- ft_memset.c
+++ ft_memset.c
@@ -1,1 +1,16 @@
- 
+#include <stddef.h>
+
+void *ft_memset(void *s, int c, size_t n)
+{
+    unsigned char *ptr;
+    size_t i;
+
+    ptr = (unsigned char *)s;
+    i = 0;
+    while (i < n)
+    {
+        ptr[i] = (unsigned char)c;
+        i++;
+    }
+    return (s);
+}
