c8a46c449680f6e73a77bba4b2023b5bbca99e16
Jun Hong <junh161108@gmail.com>
2026-08-04 22:06:27 +0800

    .

---
+++ ft_substr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/04 14:37:19 by juho              #+#    #+#             */
+/*   Updated: 2026/08/04 21:36:37 by juho             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	
+	char *ptr;
+	char *pt1;
+	size_t i;
+	size_t j;
+
+	i = start;
+	ptr = malloc(len + 1);
+	pt1 = (char *)s;
+	i = 0;
+	
+	while (i < len)
+	{
+		ptr[j] =  pt1[i];
+		i++;
+		j++;
+	} 
+	return(ptr + '\0');
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(void)
+ {
+	// char *s = av[1];
+	// int start = atoi(av[2]);
+	// int len = atoi(av[3]);
+
+	printf("%s\n", ft_substr("hello", 0, 3));
+ }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 14:00:41 by juho              #+#    #+#             */
-/*   Updated: 2026/08/03 23:15:54 by juho             ###   ########.fr       */
+/*   Updated: 2026/08/04 12:55:12 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 # include <stdio.h>
 # include <string.h>
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_isdigit(int nb);
-int	ft_isascii(int c);
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isprint(int c);
-void			ft_bzero(void *s, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isdigit(int nb);
+int		ft_isascii(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+void	ft_bzero(void *s, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -38,6 +38,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s);
 
