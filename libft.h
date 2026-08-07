c5edb05915908bb7e6a9dcd148a934b73f6cf8df
Jun Hong <junh161108@gmail.com>
2026-08-08 00:56:58 +0800

    .

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 14:00:41 by juho              #+#    #+#             */
-/*   Updated: 2026/08/06 20:35:09 by juho             ###   ########.fr       */
+/*   Updated: 2026/08/08 00:25:29 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 int		ft_atoi(const char *str);
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strtrim(char const *s1, char const *set);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_isdigit(int nb);
