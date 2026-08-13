6b5ba59d8f015e36fd732c37584d407b632caa20
Jun Hong <junh161108@gmail.com>
2026-08-13 23:14:57 +0800

    .

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/13 17:07:52 by juho              #+#    #+#             */
-/*   Updated: 2026/08/13 23:11:21 by juho             ###   ########.fr       */
+/*   Created: 2026/08/13 22:28:18 by juho              #+#    #+#             */
+/*   Updated: 2026/08/13 23:12:45 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	write(fd, &c, 1);
+	long int	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr_fd(nb / 10, fd);
+	ft_putchar_fd(nb % 10 + '0', fd);
 }
