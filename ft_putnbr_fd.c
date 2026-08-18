2162098383016fe7e4e7e7c204f0d71a7189a2d8
Jun Hong <junh161108@gmail.com>
2026-08-18 14:30:11 +0800

    somehow putnbr got deleted so added it back

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/18 14:15:36 by juho              #+#    #+#             */
+/*   Updated: 2026/08/18 14:29:22 by juho             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long int	number;
+
+	number = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-',fd);
+		number = number *-1;
+	}
+	if (number >= 10)
+		ft_putnbr_fd(number / 10, fd);
+	else
+		ft_putchar_fd(number % 10 + '0', fd);
+}
--- ft_split.c
+++ ft_split.c
@@ -5,14 +5,69 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/10 15:48:29 by juho              #+#    #+#             */
-/*   Updated: 2026/08/15 15:01:26 by juho             ###   ########.fr       */
+/*   Created: 2026/08/18 14:14:22 by juho              #+#    #+#             */
+/*   Updated: 2026/08/18 14:14:38 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_isdelimiter(char c, char delimiter)
+{
+	return (c == delimiter);
+}
+
+static size_t	ft_count_words(char const *s, char delimiter)
+{
+	size_t	count;
+	size_t	i;
+
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (ft_isdelimiter(s[i], delimiter))
+			i++;
+		if (s[i])
+		{
+			count++;
+			while (s[i] && !ft_isdelimiter(s[i], delimiter))
+				i++;
+		}
+	}
+	return (count);
+}
+
+static char	*ft_get_word(char const *s, char d, size_t *i)
+{
+	size_t	start;
+
+	start = *i;
+	while (s[*i] && !ft_isdelimiter(s[*i], d))
+		(*i)++;
+	return (ft_substr(s, start, *i - start));
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**arr;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s)
+		return (NULL);
+	arr = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!arr)
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] && ft_isdelimiter(s[i], c))
+			i++;
+		if (s[i])
+			arr[j++] = ft_get_word(s, c, &i);
+	}
+	arr[j] = NULL;
+	return (arr);
 }
