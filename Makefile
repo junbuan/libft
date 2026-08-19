4a712f5dec7224b8a597d0c33e88c1be778850a9
Jun Hong <junh161108@gmail.com>
2026-08-19 17:22:28 +0800

    .

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/08/10 16:15:28 by juho              #+#    #+#              #
-#    Updated: 2026/08/19 15:33:14 by juho             ###   ########.fr        #
+#    Updated: 2026/08/19 16:48:12 by juho             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,10 +26,9 @@ OBJS_B = $(CFILES_BONUS:.c=.o)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 all: ${NAME}
-	$(NORM)
 
-$(NAME): $(OBJS) $(OBJS_B)
-	$(LIBC) $(NAME) $(OBJS) $(OBJS_B)
+$(NAME): $(OBJS)
+	$(LIBC) $(NAME) $(OBJS)
 
 bonus: $(OBJS) $(OBJS_B)
 	$(LIBC) $(NAME) $(OBJS) $(OBJS_B)
--- ft_split.c
+++ ft_split.c
@@ -6,8 +6,60 @@
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 14:14:22 by juho              #+#    #+#             */
-/*   Updated: 2026/08/19 15:25:10 by juho             ###   ########.fr       */
+/*   Updated: 2026/08/19 16:35:28 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	isdelimiter(char c, char delimiter)
+{
+	return (c == delimiter);
+}
+static int	count_word(char const	*s, char delimiter)
+{
+	int	count;
+	int i;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (isdelimiter(s[i], delimiter))
+			i++;
+		if (s[i])
+		{
+			count++;
+			while (s[i] && !isdelimiter(s[i], delimiter))
+				i++;
+		}
+	}
+	return (count);
+}
+char **ft_split(char const *s, char c)
+{
+	int	i;
+	int	j;
+	char **ptr;
+	int count;
+
+	count = count_word(s, c);
+	*ptr = malloc(sizeof(char *) * (count_word + 1));
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+		{
+			i++;
+		}
+		j = i;
+		while (s[i] != c && s[i])
+		{
+			i++;
+		}
+		count = 0;
+		while (count < count_word(s,c))
+			ptr[count++] = malloc(sizeof(i - j));
+	}
+}
